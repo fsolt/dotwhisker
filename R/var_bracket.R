a1e9203f1b9752fe37570ce14b040a1657db24fc
Frederick Solt <fsolt@mac.com>
2015-07-15 10:53:31 -0500

    comment out so package builds

--- R/var_bracket.R
+++ R/var_bracket.R
@@ -1,8 +1,10 @@
-theme(plot.margin = unit(c(1, 1, 1, 1.3), "lines")) +
-    annotation_custom(
-        grob = textGrob(label = "Individual Level", gp = gpar(cex = .7, fontface="italic"), rot=90),
-        ymin = 5, ymax = 5,
-        xmin = -7.8, xmax = -7.8) +
-    annotation_custom(grob = linesGrob(), xmin = -7.5, xmax = -7.5, ymin = .7, ymax = 9.4) +
-    annotation_custom(grob = linesGrob(), xmin = -7.5, xmax = -7, ymin = 9.4, ymax = 9.4) +
-    annotation_custom(grob = linesGrob(), xmin = -7.5, xmax = -7, ymin = .7, ymax = .7)
+# library(grid)
+#
+# theme(plot.margin = unit(c(1, 1, 1, 1.3), "lines")) +
+#     annotation_custom(
+#         grob = textGrob(label = "Individual Level", gp = gpar(cex = .7, fontface="italic"), rot=90),
+#         ymin = 5, ymax = 5,
+#         xmin = -7.8, xmax = -7.8) +
+#     annotation_custom(grob = linesGrob(), xmin = -7.5, xmax = -7.5, ymin = .7, ymax = 9.4) +
+#     annotation_custom(grob = linesGrob(), xmin = -7.5, xmax = -7, ymin = 9.4, ymax = 9.4) +
+#     annotation_custom(grob = linesGrob(), xmin = -7.5, xmax = -7, ymin = .7, ymax = .7)
