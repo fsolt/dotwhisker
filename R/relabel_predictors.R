2cf17003ef3cde63d464e922ec5abec37000d535
Frederick Solt <fsolt@mac.com>
2015-08-12 13:05:04 -0500

    add relabel_predictors; revise relabel_y_axis

---
+++ R/relabel_predictors.R
@@ -0,0 +1,27 @@
+#' Relabel the Predictors in a Tidy Data Frame of Regression Results
+#'
+#' \code{relabel_predictors} is a convenience function for relabeling the predictors in a tidy data.frame to be passed to \code{\link[dotwhisker]{dwplot}}
+#'
+#' @param df A tidy data.frame to be passed to \code{\link[dotwhisker]{dwplot}}
+#' @param replace A named character vector, with new values as values, and old values as names
+#'
+#' @examples
+#' data(mtcars)
+#' m1 <- lm(mpg ~ wt + cyl + disp, data = mtcars)
+#' m1_df <- tidy(m1) %>%
+#'          relabel_predictors(c("(Intercept)" = "Intercept",
+#'                               wt = "Weight",
+#'                               cyl = "Cylinder",
+#'                               disp = "Displacement"))
+#' dwplot(m1_df)
+#'
+#' @seealso \code{\link[dotwhisker]{relabel_y_axis}} to relabel the predictors on the y-axis of a dot-whisker plot after using \code{\link[dotwhisker]{dwplot}}
+#'
+#' @importFrom plyr revalue
+#'
+#' @export
+
+relabel_predictors <- function(df, replace = NULL) {
+    df$term <- plyr::revalue(df$term, replace = replace)
+    df
+}
--- R/relabel_y_axis.R
+++ R/relabel_y_axis.R
@@ -1,13 +1,15 @@
-#' Relabel Y Axis
+#' Relabel the Y-Axis of a Dot-Whisker Plot
 #'
-#' \code{relabel_y_axis} is a convenience function for relabeling the predictors of a dot-whisker plot
+#' \code{relabel_y_axis} is a convenience function for relabeling the predictors on the y-axis of a dot-whisker plot created by \code{\link[dotwhisker]{dwplot}}
 #'
 #' @param x A vector of labels for predictors, listed from top to bottom
+#'
+#' @seealso \code{\link[dotwhisker]{relabel_predictors}} to relabel the predictors in a tidy data.frame before using \code{\link[dotwhisker]{dwplot}}
 #'
 #' @examples
 #' data(mtcars)
 #' m1 <- lm(mpg ~ wt + cyl + disp, data = mtcars)
-#' p <- dwplot(m1) + relabel_y_axis(c("Intercept", "Weight", "Cylinders", "Displacement"))
+#' dwplot(m1) + relabel_y_axis(c("Intercept", "Weight", "Cylinders", "Displacement"))
 #'
 #' @export
 
