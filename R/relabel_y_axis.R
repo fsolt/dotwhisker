0bfa821a888b46fa275db992eb35d32ff844e4e1
Frederick Solt <fsolt@mac.com>
2015-08-12 11:27:27 -0500

    add relabel_y_axis

---
+++ R/relabel_y_axis.R
@@ -0,0 +1,16 @@
+#' Relabel Y Axis
+#'
+#' \code{relabel_y_axis} is a convenience function for relabeling the predictors of a dot-whisker plot
+#'
+#' @param x A vector of labels for predictors in a dot-whisker plot, listed from top to bottom
+#'
+#' @examples
+#' data(mtcars)
+#' m1 <- lm(mpg ~ wt + cyl + disp, data = mtcars)
+#' p <- dwplot(m1) + relabel_y_axis(c("Weight", "Cylinders", "Displacement"))
+#'
+#' @export
+
+relabel_y_axis <- function(x) {
+    scale_y_discrete(breaks = length(x):1, labels = x)
+}
