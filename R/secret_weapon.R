75594597f6bd84c7b5af9876419701721c25d7c2
Frederick Solt <fsolt@mac.com>
2015-08-16 17:05:06 -0500

    revise documentation

--- R/secret_weapon.R
+++ R/secret_weapon.R
@@ -1,13 +1,13 @@
 #' Format Regression Results for a 'Secret Weapon' Plot
 #'
-#' \code{relabel_predictors} is a function for formatting regression results to be passed to \code{\link[dotwhisker]{dwplot}} to generate a 'secret weapon' plot
+#' \code{secret_weapon} is a function for formatting regression results to be passed to \code{\link[dotwhisker]{dwplot}} to generate a 'secret weapon' plot
 #'
 #' @param x Either a tidy data.frame including results from multiple models (see 'Details') or a list of model objects that can be tidied with \code{\link[broom]{tidy}}
 #' @param var The predictor whose results are to be shown in the 'secret weapon' plot
 #'
 #' @details
 #' Andrew Gelman has coined the term \href{http://andrewgelman.com/2005/03/07/the_secret_weap/}{"the secret weapon"} for dot-and-whisker plots that compare the estimated coefficients for a single predictor across many models or datasets.
-#' \code{sw_format} takes a tidy data.frame of regression results suitable for passing to \code{\link[dotwhisker]{dwplot}} or a list of model objects and reformats these results so that, when passed to \code{\link[dotwhisker]{dwplot}}, the result will be a dot-and-whisker plot of the results of a single variable across the multiple models.
+#' \code{secret_weapon} takes a tidy data.frame of regression results suitable for passing to \code{\link[dotwhisker]{dwplot}} or a list of model objects and reformats these results so that, when passed to \code{\link[dotwhisker]{dwplot}}, the result will be a dot-and-whisker plot of the results of a single variable across the multiple models.
 #'
 #' @examples
 #' # Estimate models across many samples, put results in a tidy data.frame
@@ -16,16 +16,15 @@
 #'  ungroup %>% rename(model=clarity)
 #'
 #' # Format for a 'secret weapon' plot of the results of diamond size, make the plot
-#' sw_format(by_clarity, "carat") %>% dwplot
+#' secret_weapon(by_clarity, "carat") %>% dwplot
 #'
-#' @seealso \code{\link[dotwhisker]{relabel_y_axis}} to relabel the predictors on the y-axis of a dot-whisker plot after using \code{\link[dotwhisker]{dwplot}}
 #'
 #' @importFrom broom tidy
 #' @importFrom dplyr filter select rename
 #'
 #' @export
 
-sw_format <- function(x, var) {
+secret_weapon <- function(x, var) {
     # If x is model object(s), convert to a tidy data.frame
     if (!is.data.frame(x)) {
         if (is.list(x)) {
