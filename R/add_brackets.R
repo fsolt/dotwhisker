04cef7b566b33283333f8b69fef65984025c5491
Frederick Solt <fsolt@mac.com>
2018-03-30 08:25:32 -0500

    fix alignment issue: use p1 as basis of p2, and use cowplot::plot_grid() to arrange. Closes #77

--- R/add_brackets.R
+++ R/add_brackets.R
@@ -19,8 +19,8 @@
 #'     relabel_predictors(c("(Intercept)" = "Intercept",
 #'                      wt = "Weight",
 #'                      cyl = "Cylinder",
-#'                      disp = "Displacement"))#'
-
+#'                      disp = "Displacement"))
+#'
 #' p <- dwplot(m1_df, include_intercept = TRUE) +
 #'    theme_bw() + xlab("Coefficient") + ylab("") +
 #'    geom_vline(xintercept = 0, colour = "grey50", linetype = 2) +
@@ -29,70 +29,89 @@
 #' two_brackets <- list(c("Engine", "Cylinder", "Displacement"),
 #' c("Not Engine", "Intercept", "Weight"))
 #' g <- p %>% add_brackets(two_brackets)
-#' grid.arrange(g)  # to display
+#' g  # to display
 #'
 #' # to save (not run)
-#' #ggsave(file = "plot.pdf", g)
+#' # ggsave(file = "plot.pdf", g)
 #'
 #' @import gtable
 #' @importFrom grid textGrob linesGrob gpar
-#' @importFrom gridExtra grid.arrange
+#' @importFrom cowplot plot_grid
+#' @importFrom dplyr filter pull
 #'
 #' @export
 
 add_brackets <- function(p, brackets, face="italic") {
-  pd <- p$data
+  pd <- left_join(p$data, layer_data(p), by = c("estimate" = "x"))
   y_ind <- NULL # not functional, just for CRAN check
   overhang <- max(pd$y_ind)/40
-  overhang <- ifelse(overhang>.4, .4, overhang)
+  overhang <- ifelse(overhang > .4, .4, overhang)
+  farout <- max(pd$xmax, na.rm = TRUE) + 100
   p1 <- p + theme(plot.margin = unit(c(1, 1, 1, -1), "lines")) + ylab("")
 
   if (!is.list(brackets)) stop('Error: argument "brackets" is not a list')
 
   draw_bracket_label <- function(x, f = face) {
-      v1 <- pd$y_ind[which(unique(pd$term)==x[2])]
-      v2 <- pd$y_ind[which(unique(pd$term)==x[3])]
-      top <- max(v1, v2)
-      bottom <- min(v1, v2)
+      top <- pd %>% filter(term == x[2] | term == x[3] & !is.na(estimate)) %>% pull(ymax) %>% max()
+      bottom <- pd %>% filter(term == x[2] | term == x[3] & !is.na(estimate)) %>% pull(ymin) %>% min()
+      shift <- max(abs(top - round(top)), abs(round(bottom) - bottom))
+      top <- round(top) + shift
+      bottom <- round(bottom) - shift
+
       annotation_custom(
           grob = textGrob(label = x[1], gp = gpar(cex = .7, fontface = f), rot = 90),
-          ymin = (top+bottom)/2, ymax = (top+bottom)/2,
-          xmin = .3, xmax = .3)
+          ymin = (top + bottom)/2, ymax = (top + bottom)/2,
+          xmin = farout, xmax = farout)
   }
 
   draw_bracket_vert <- function(x, oh = overhang) {
-      v1 <- pd$y_ind[which(unique(pd$term)==x[2])]
-      v2 <- pd$y_ind[which(unique(pd$term)==x[3])]
-      top <- max(v1, v2)
-      bottom <- min(v1, v2)
-      annotation_custom(grob = linesGrob(), xmin = .6, xmax = .6, ymin = bottom-oh, ymax = top+oh)
+      top <- pd %>% filter(term == x[2] | term == x[3] & !is.na(estimate)) %>% pull(ymax) %>% max()
+      bottom <- pd %>% filter(term == x[2] | term == x[3] & !is.na(estimate)) %>% pull(ymin) %>% min()
+      shift <- max(abs(top - round(top)), abs(round(bottom) - bottom))
+      top <- round(top) + shift
+      bottom <- round(bottom) - shift
+
+      annotation_custom(grob = linesGrob(), xmin = farout + 0.5, xmax = farout + 0.5, ymin = bottom - oh, ymax = top + oh)
   }
 
   draw_bracket_top <- function(x, oh = overhang) {
-      v1 <- pd$y_ind[which(unique(pd$term)==x[2])]
-      v2 <- pd$y_ind[which(unique(pd$term)==x[3])]
-      top <- max(v1, v2)
-      annotation_custom(grob = linesGrob(), xmin = .6, xmax = 1, ymin = top+oh, ymax = top+oh)
+      top <- pd %>% filter(term == x[2] | term == x[3] & !is.na(estimate)) %>% pull(ymax) %>% max()
+      bottom <- pd %>% filter(term == x[2] | term == x[3] & !is.na(estimate)) %>% pull(ymin) %>% min()
+      shift <- max(abs(top - round(top)), abs(round(bottom) - bottom))
+      top <- round(top) + shift
+      bottom <- round(bottom) - shift
+
+      annotation_custom(grob = linesGrob(), xmin = farout + 0.5, farout + 1, ymin = top + oh, ymax = top + oh)
   }
 
   draw_bracket_bottom <- function(x, oh = overhang) {
-      v1 <- pd$y_ind[which(unique(pd$term)==x[2])]
-      v2 <- pd$y_ind[which(unique(pd$term)==x[3])]
-      bottom <- min(v1, v2)
-      annotation_custom(grob = linesGrob(), xmin = .6, xmax = 1, ymin = bottom-oh, ymax = bottom-oh)
+      top <- pd %>% filter(term == x[2] | term == x[3] & !is.na(estimate)) %>% pull(ymax) %>% max()
+      bottom <- pd %>% filter(term == x[2] | term == x[3] & !is.na(estimate)) %>% pull(ymin) %>% min()
+      shift <- max(abs(top - round(top)), abs(round(bottom) - bottom))
+      top <- round(top) + shift
+      bottom <- round(bottom) - shift
+
+      annotation_custom(grob = linesGrob(), xmin = farout + 0.5, xmax = farout + 1, ymin = bottom - oh, ymax = bottom - oh)
   }
 
-  theme_p2 <- theme_bw()
-  theme_p2$line <- element_blank()
-  theme_p2$rect <- element_blank()
-
-
-  n_vars <- length(unique(p$data$term))
-  p2 <- ggplot(p$data, aes(x = -1, y = y_ind)) + geom_point() +
-      coord_cartesian(ylim = c(.5, n_vars+.5), xlim = c(0, 1)) +
-      xlab(p$labels$x) + ylab("") + theme_p2 +
-      theme(plot.margin = unit(c(1, -.5, 1, 0), "lines")) +
-      scale_x_continuous(expand = c(0,0))
+  p2 <- p1 +
+      theme(plot.title = element_text(colour = NA),
+            axis.title.y = element_blank(),
+            axis.title.x = element_text(colour = NA),
+            axis.text.y = element_blank(),
+            axis.text.x = element_text(colour = NA),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            axis.ticks = element_line(colour = NA),
+            panel.border = element_rect(colour = NA),
+            strip.background = element_rect(colour = NA, fill = NA),
+            strip.text.x = element_text(colour = NA),
+            plot.margin = unit(c(1,0,2,0), "lines"),
+            legend.background = element_rect(colour = NA),
+            legend.key = element_rect(colour = NA),
+            legend.text = element_text(colour = NA)) +
+      coord_cartesian(xlim = c(farout - 1, farout + 1)) +
+      theme(legend.position = "none")
 
   for (i in seq(length(brackets))) {
       p2 <- p2 +
@@ -102,13 +121,7 @@ add_brackets <- function(p, brackets, face="italic") {
           draw_bracket_bottom(brackets[[i]])
   }
 
-  g1 <- gtable_add_cols(ggplotGrob(p1), unit(1, "cm"), 0)
-  g1[["layout"]]$l[g1[["layout"]]$name=="background"] <- 1
-  g2 <- gtable_filter(ggplotGrob(p2), pattern = "panel")
-
-  g <- gtable_add_grob(g1, g2, g1[["layout"]]$t[g1[["layout"]]$name=="panel"], 1)
-  g <- gtable_add_cols(g, unit(.7, "line"), 0)
-  g[["layout"]]$l[g[["layout"]]$name=="background"] <- 1
+  g <- cowplot::plot_grid(p2, p1, align = "h", rel_widths = c(1, 8))
 
   return(g)
 }
